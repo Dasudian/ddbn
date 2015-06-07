07c77cf080c9097a52826ed0accc3dfd3345e9ba
Ivan Uemlianin <ivan@llaisdy.com>
2015-06-07 21:32:22 +0100

    Bringing bn_tests.erl in line with wiki.

--- src/bn_tests.erl
+++ src/bn_tests.erl
@@ -2,36 +2,33 @@
 
 -module(bn_tests).
 -include("types.hrl").
--export([all/0, cgp/0, pgc/3, p/1]).
+-export([all/0, child_given_parents/0, parent_given_child/3, p/1]).
 
 -compile([export_all]).
 
 all() ->
-    cgp(),
-    pgc(sp, st, n),
-    pgc(sp, st, y),
-    pgc(fu, st, n),
-    pgc(fu, st, y),
+    child_given_parents(),
+    parent_given_child(sp, st, n),
+    parent_given_child(sp, st, y),
+    parent_given_child(fu, st, n),
+    parent_given_child(fu, st, y),
     ok.    
 
-cgp() ->
+child_given_parents() ->
     Parents = [p(fu), p(sp)],
     Child = p(st),
-    {st,Exp} = {st,[{n,0.06821599999999998},{y,0.931784}]},
+    {st,Exp} = child_given_parents_check(),
     {st,Act} = bn:child_given_parents(Child, Parents),
     similar_kvlist(Exp, Act).
 
-pgc(Plab, Clab, Cval) ->
+parent_given_child(Plab, Clab, Cval) ->
     Parents = [p(fu), p(sp)],
     CP = p(Clab),
-    {Plab,Exp} = pgc_check(Plab, Clab, Cval),
+    {Plab,Exp} = parent_given_child_check(Plab, Clab, Cval),
     {Plab,Act} = bn:parent_given_child(CP, Parents, Plab, Cval),
     similar_kvlist(Exp, Act).
 
 %%%% private
-
-ofl(L) ->
-     orddict:from_list(L).
 
 -spec p(atom()) -> probability().
 p(fu) ->
@@ -47,14 +44,21 @@ p(st) ->
 	  {ofl([{fu,n},{sp,n}]), ofl([{y,0.0},{n,1.0}])}
 	 ]}.
 
-pgc_check(sp, st, n) ->
-    {sp,[{n,0.5806262460419842},{y,0.4193737539580157}]};
-pgc_check(fu, st, n) ->
+child_given_parents_check() ->
+    {st,[{n,0.06821599999999998},{y,0.931784}]}.
+
+parent_given_child_check(fu, st, n) ->
     {fu,[{n,0.2931863492435792},{y,0.7068136507564208}]};
-pgc_check(fu, st, y) ->
+parent_given_child_check(fu, st, y) ->
     {fu,[{n,0.0},{y,1.0}]};
-pgc_check(sp, st, y) ->
+parent_given_child_check(sp, st, n) ->
+    {sp,[{n,0.5806262460419842},{y,0.4193737539580157}]};
+parent_given_child_check(sp, st, y) ->
     {sp,[{n,4.206983592763989e-4},{y,0.9995793016407236}]}.
+
+
+ofl(L) ->
+     orddict:from_list(L).
 
 similar_kvlist(Xs, Ys) ->
     all_true(lists:zipwith(fun({K1,X}, {K2,Y}) ->
